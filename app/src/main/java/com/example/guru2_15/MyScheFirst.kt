c447e908bd4693bf7974f9540232f00d61c2c215
Rachel Kim <hyunjinney@naver.com>
2022-07-25 22:54:15 +0900

    김현진_first commit
    첫번째 합치기

---
+++ app/src/main/java/com/example/guru2_15/MyScheFirst.kt
@@ -0,0 +1,21 @@
+package com.example.guru2_15
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MyScheFirst : AppCompatActivity() {
+
+    lateinit var addScheBtn : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_sche_first)
+
+        addScheBtn = findViewById(R.id.addScheBtn)
+
+        addScheBtn.setOnClickListener {
+
+        }
+    }
+}
