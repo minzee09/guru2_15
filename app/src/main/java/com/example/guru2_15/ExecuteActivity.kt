b51fe4c2533ef01173596e4d264f046772f4ff49
yoonsu Kim <iamkys0417@gmail.com>
2022-07-27 01:30:15 +0900

    calendar

--- app/src/main/java/com/example/guru2_15/ExecuteActivity.kt
+++ app/src/main/java/com/example/guru2_15/ExecuteActivity.kt
@@ -3,41 +3,100 @@ package com.example.guru2_15
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.CalendarView
+import android.util.Log
+import android.view.View
+import android.widget.*
+import androidx.core.view.get
+import org.w3c.dom.Text
 import java.util.*
 
 class ExecuteActivity : AppCompatActivity() {
+
+    var selectYear : Int = 0
+    var selectMonth : Int = 0
+    var selectDay : Int = 0
+
+    var selectYear2 : Int = 0
+    var selectMonth2 : Int = 0
+    var selectDay2 : Int = 0
+
+    var stHour : Int = 0
+    var stMinute : Int = 0
+
+    var fHour : Int = 0
+    var fMinute : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_execute)
+        setContentView(R.layout.activity_main)
         setTitle("약속 잡기")
 
         lateinit var btnmake : Button
         lateinit var calendarView : CalendarView
+        lateinit var calendarView2 : CalendarView
 
-        var selectYear : Int = 0
-        var selectMonth : Int = 0
-        var selectDay : Int = 0
-        var selectHour : Int = 0
-        var selectMinute : Int = 0
+
+        val stHour = findViewById<Spinner>(R.id.stHour)
+        val stMinute = findViewById<Spinner>(R.id.stMinute)
+
+        val fHour = findViewById<Spinner>(R.id.fHour)
+        val fMinute = findViewById<Spinner>(R.id.fMinute)
+
+        stHour.adapter = ArrayAdapter.createFromResource(this, R.array.hour_list, android.R.layout.simple_spinner_dropdown_item)
+        stMinute.adapter = ArrayAdapter.createFromResource(this, R.array.minute_list, android.R.layout.simple_spinner_dropdown_item)
+
+        fHour.adapter = ArrayAdapter.createFromResource(this, R.array.hour_list, android.R.layout.simple_spinner_dropdown_item)
+        fMinute.adapter = ArrayAdapter.createFromResource(this, R.array.minute_list, android.R.layout.simple_spinner_dropdown_item)
 
         btnmake = findViewById(R.id.btnmake)
+        calendarView = findViewById(R.id.calenderView)
+        calendarView2 = findViewById(R.id.calenderView2)
 
-//        calendarView.setOnDateChangeListener { calendarView, year, month, day ->
-//            selectYear = year
-//            selectMonth = month
-//            selectDay = day
-//        }
 
+        calendarView.setOnDateChangeListener { view, i, i2, i3 ->
+            this.selectYear = i
+            this.selectMonth = i2+1
+            this.selectDay = i3
+
+        }
+
+        calendarView2.setOnDateChangeListener { view, i, i2, i3 ->
+            this.selectYear2 = i
+            this.selectMonth2 = i2+1
+            this.selectDay2 = i3
+        }
 
         btnmake.setOnClickListener {
-            val intent = Intent(this, ResultActivity::class.java)
-            intent.putExtra("stMon", Integer.toString(selectMonth))
-            intent.putExtra("stDay", Integer.toString(selectDay))
-            intent.putExtra("stYear", Integer.toString(selectYear))
-            startActivity(intent)
+            if(selectYear == 0 )
+            {
+                Toast.makeText(this, "시작 날짜를 선택해주세요", Toast.LENGTH_SHORT).show()
+            }
+            else if(selectYear2 == 0)
+            {
+                Toast.makeText(this, "종료 날짜를 선택해주세요", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val intent = Intent(this, ResultActivity::class.java)
 
+                intent.putExtra("stYear", Integer.toString(selectYear))
+                intent.putExtra("stMon", Integer.toString(selectMonth))
+                intent.putExtra("stDay", Integer.toString(selectDay))
+
+                intent.putExtra("fYear", Integer.toString(selectYear2))
+                intent.putExtra("fMon", Integer.toString(selectMonth2))
+                intent.putExtra("fDay", Integer.toString(selectDay2))
+
+                intent.putExtra("stHour", stHour.selectedItem.toString())
+                intent.putExtra("stMinute", stMinute.selectedItem.toString())
+
+                intent.putExtra("fHour", fHour.selectedItem.toString())
+                intent.putExtra("fMinute", fMinute.selectedItem.toString())
+
+//            Log.d("intentday", selectMonth.toString())
+//            Log.d("intentday", selectDay.toString())
+
+                startActivity(intent)
+            }
         }
     }
 }
--- app/src/main/java/com/example/guru2_15/ResultActivity.kt
+++ app/src/main/java/com/example/guru2_15/ResultActivity.kt
@@ -2,9 +2,11 @@ package com.example.guru2_15
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
 
 class ResultActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,27 +21,37 @@ class ResultActivity : AppCompatActivity(){
         lateinit var stDay : TextView
         lateinit var stHour : TextView
         lateinit var stMinute: TextView
+
+        lateinit var fYear : TextView
         lateinit var fMon : TextView
         lateinit var fDay : TextView
         lateinit var fHour : TextView
         lateinit var fMinute : TextView
 
-//        val secondintent = intent
-//
-//        if(secondintent.hasExtra("stYear"))
-//        {
-//            stYear.text = secondintent.getStringExtra("stYear")
-//        }
-//
-//        if(secondintent.hasExtra("stMon"))
-//        {
-//            stMon.text = secondintent.getStringExtra("stMon")
-//        }
-//
-//        if(secondintent.hasExtra("stDay"))
-//        {
-//            stDay.text = secondintent.getStringExtra("stDay")
-//        }
+        stYear = findViewById(R.id.stYear)
+        stMon = findViewById(R.id.stMon)
+        stDay = findViewById(R.id.stDay)
+        stHour = findViewById(R.id.stHour)
+        stMinute = findViewById(R.id.stMinute)
+
+        fYear = findViewById(R.id.fYear)
+        fMon = findViewById(R.id.fMon)
+        fDay = findViewById(R.id.fDay)
+        fHour = findViewById(R.id.fHour)
+        fMinute = findViewById(R.id.fMinute)
+
+
+        stYear.setText(intent.getStringExtra("stYear"))
+        stMon.setText(intent.getStringExtra("stMon"))
+        stDay.setText(intent.getStringExtra("stDay"))
+        stHour.setText(intent.getStringExtra("stHour"))
+        stMinute.setText(intent.getStringExtra("stMinute"))
+
+        fYear.setText(intent.getStringExtra("fYear"))
+        fMon.setText(intent.getStringExtra("fMon"))
+        fDay.setText(intent.getStringExtra("fDay"))
+        fHour.setText(intent.getStringExtra("fHour"))
+        fMinute.setText(intent.getStringExtra("fMinute"))
 
         btnRe = findViewById(R.id.btnRe)
 
