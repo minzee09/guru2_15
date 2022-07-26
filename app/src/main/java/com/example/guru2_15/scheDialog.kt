9c498aba7b3846137c029fe745facfe0dec2d513
Rachel Kim <hyunjinney@naver.com>
2022-07-26 09:06:20 +0900

    commit

---
+++ app/src/main/java/com/example/guru2_15/scheDialog.kt
@@ -0,0 +1,39 @@
+package com.example.guru2_15
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import android.app.Dialog
+import android.content.Context
+import android.view.WindowManager
+import android.widget.Button
+
+
+class scheDialog(context: Context) {
+    private val dialog = Dialog(context)
+    private lateinit var onClickListener: OnDialogClickListener
+
+
+    fun setOnClickListener(listener: OnDialogClickListener)
+    {
+        onClickListener = listener
+    }
+    fun showDialog()
+    {
+        dialog.setContentView(R.layout.activity_sche_dialog)
+        dialog.window!!.setLayout(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT)
+        dialog.setCanceledOnTouchOutside(true)
+        dialog.setCancelable(true)
+        dialog.show()
+
+        val cancleBtn = dialog.findViewById<Button>(R.id.cancelBtn)
+        cancleBtn.setOnClickListener {
+            dialog.dismiss()
+        }
+    }
+    interface OnDialogClickListener
+    {
+        fun onClicked(name: String)
+    }
+
+}
